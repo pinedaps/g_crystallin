2792b77a1acaf33552d323941b828dcdaf7042c4
Sebastian Pineda <sebastian@dell7050>
2025-11-27 18:10:47 +0100

    Added arg.parse to choose either standard or custumized plotting setup (colors, markers)

--- plots/plot_potential.py
+++ plots/plot_potential.py
@@ -16,28 +16,40 @@ parser = argparse.ArgumentParser(description='Configuration of data source')
 parser.add_argument('path', metavar='Path', type=str, nargs='?',
                     default='../results/dat/',
                     help='path where data is stored (default: ../results/dat/)')
+parser.add_argument('plot_setup', metavar='plot_colors_markers', type=str, nargs='?',
+                    default='standard',
+                    help='Colors and markers for the plot (default: standar = random colors and o marker)')
 args = parser.parse_args()
+
+plt_setup = args.plot_setup
+
 data_dir = args.path
 
 # Create the list of files
 
 files = sorted(np.array( [name for name in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, name))]), reverse=True)
 
+print('Files to be processed:')
+print(files)
+
 # Create the list of colors and fillstyles to facilitate comparison 
 
 colors  = ['black','black','black','blue','green','red','green','red','red','red','red','orange','purple', 'purple'] 
 markers = ['^',    '*',    'o',    'o',   'o',    'o',  '*',    '*',  'v',  's',  '>',  'o',     '*'    , 'o']
 
-fig, ax1 = plt.subplots(nrows=1)
+fig, ax = plt.subplots(nrows=1)
 
 for index,file in enumerate(files):
+	plt.axhline(y=0, color='black', lw=1, ls='--', alpha=0.5)
 	print('Procesing file: ', file )
 	R, F, U, C, exp = np.loadtxt(data_dir+file, unpack=True)
-	ax1.plot(R, U, ms=5, marker=markers[index], fillstyle='none', lw=1, color=colors[index], label=file.replace('pmf_','').replace('.dat',''))
-	
-plt.ylim(-50,50)
+	if plt_setup == 'standard':
+		ax.plot(R, U, ms=5, marker='o', lw=1, label=file.replace('pmf_','').replace('.dat',''))
+	else:	
+		ax.plot(R, U, ms=5, marker=markers[index], lw=1, color=colors[index], label=file.replace('pmf_','').replace('.dat',''))
+plt.ylim(-11,5)
 plt.xlim(23,60)
-plt.xlabel(r"Radius [$\AA$]")
-plt.ylabel(r"Potential, U [$k_BT$]")
-plt.legend(ncol=2)
+plt.xlabel(r"Distance,  r [$\AA$]")
+plt.ylabel(r"Potential,  U [$k_BT$]")
+plt.legend(ncol=1, title='T [K]')
 plt.savefig("./tmp_fig/potential.png")
