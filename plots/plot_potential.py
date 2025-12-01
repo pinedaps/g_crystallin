61aecfedff4a48d3eb293d8b700d05d0f4aa4b82
Sebastian Pineda <sebastian@dell7050>
2025-12-01 09:37:39 +0100

    Expansion of the plotting script to plot the B2

--- plots/plot_potential.py
+++ plots/plot_potential.py
@@ -4,6 +4,7 @@ from scipy.interpolate import griddata
 from math import ceil
 import argparse
 import os
+import json
 import common as cmn
 
 #################################### Loading plotting features ##################################
@@ -14,8 +15,7 @@ cmn
 
 parser = argparse.ArgumentParser(description='Configuration of data source')
 parser.add_argument('path', metavar='Path', type=str, nargs='?',
-                    default='../results/dat/',
-                    help='path where data is stored (default: ../results/dat/)')
+                    help='path where data is stored')
 parser.add_argument('plot_setup', metavar='plot_colors_markers', type=str, nargs='?',
                     default='standard',
                     help='Colors and markers for the plot (default: standar = random colors and o marker)')
@@ -23,14 +23,26 @@ args = parser.parse_args()
 
 plt_setup = args.plot_setup
 
-data_dir = args.path
+data_dir = args.path+'/dat/'
+json_dir = args.path+'/json/'
+plot_dir = './tmp_fig/'+args.path.split('/')[2]+'/'
 
 # Create the list of files
 
-files = sorted(np.array( [name for name in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, name))]), reverse=True)
+# Potential
+
+files_pot = sorted(np.array( [name for name in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, name))]), reverse=True)
+
+# Temperatures
+
+T = sorted(np.array( [float(name.replace('.dat','')) for name in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, name))]), reverse=True) 
+
+# B2
+
+files_b2 = sorted(np.array( [name for name in os.listdir(json_dir) if os.path.isfile(os.path.join(json_dir, name))]), reverse=True)
 
 print('Files to be processed:')
-print(files)
+print(files_pot)
 
 # Create the list of colors and fillstyles to facilitate comparison 
 
@@ -39,17 +51,31 @@ markers = ['^',    '*',    'o',    'o',   'o',    'o',  '*',    '*',  'v',  's',
 
 fig, ax = plt.subplots(nrows=1)
 
-for index,file in enumerate(files):
+for index, file_pot in enumerate(files_pot):
 	plt.axhline(y=0, color='black', lw=1, ls='--', alpha=0.5)
-	print('Procesing file: ', file )
-	R, F, U, C, exp = np.loadtxt(data_dir+file, unpack=True)
+	print('Procesing file: ', file_pot )
+	R, F, U, C, exp = np.loadtxt(data_dir+file_pot, unpack=True)
 	if plt_setup == 'standard':
-		ax.plot(R, U, ms=5, marker='o', lw=1, label=file.replace('pmf_','').replace('.dat',''))
+		ax.plot(R, U, ms=5, marker='o', lw=1, label=file_pot.replace('pmf_','').replace('.dat',''))
 	else:	
-		ax.plot(R, U, ms=5, marker=markers[index], lw=1, color=colors[index], label=file.replace('pmf_','').replace('.dat',''))
+		ax.plot(R, U, ms=5, marker=markers[index], lw=1, color=colors[index], label=file_pot.replace('pmf_','').replace('.dat',''))
 plt.ylim(-11,5)
 plt.xlim(23,60)
 plt.xlabel(r"Distance,  r [$\AA$]")
 plt.ylabel(r"Potential,  U [$k_BT$]")
 plt.legend(ncol=1, title='T [K]')
-plt.savefig("./tmp_fig/potential.png")
+plt.savefig(plot_dir+"potential.png")
+
+fig, ax = plt.subplots(nrows=1)
+
+b2 = []
+for index, file_b2 in enumerate(files_b2):
+	print('Procesing file: ', file_b2 )
+	with open(json_dir+file_b2, 'r') as file:
+		b2.append(json.load(file)['B2_reduced'])
+ax.plot(T, b2, ms=5, marker='o', lw=1, label=file_b2.replace('pmf_','').replace('.json',''))
+#plt.ylim(-11,5)
+#plt.xlim(23,60)
+plt.xlabel(r"Temperature, T [$K$]")
+plt.ylabel(r"$b_2^*=b_2/b_2^{HS}$")
+plt.savefig(plot_dir+"B2.png")
