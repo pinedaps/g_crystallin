Implementation of B2_HS calculation inside the plot script so it can produce the correct B2 reduced values, depending on radius -sigma- of the specific protein

plot_scripts/plot_potential.py
@@ -16,17 +16,25 @@ cmn
 parser = argparse.ArgumentParser(description='Configuration of data source')
 parser.add_argument('path', metavar='Path', type=str, nargs='?',
                     help='path where data is stored')
+parser.add_argument('sigma', metavar='Sigma', type=float, nargs='?',
+                    help='radius of the protein in Angtroms')
 args = parser.parse_args()
 
 data_dir = args.path
 plot_dir = args.path.replace('scans','plots')
-print(plot_dir)
+
+# Calculation of the B2 for a hard sphere of radius 'sigma'
+
+sigma = args.sigma
+B2_HS = 2/3*np.pi*sigma**3
+print('B2_HS = ' +str(B2_HS))
+
 # Extract T values from filename and create the list of files 
 
 T         = []
 files_pot = []
 files_b2  = []
-for name in os.listdir(data_dir):
+for name in sorted(os.listdir(data_dir),reverse=True):
 	if name.endswith(".dat"):
 		files_pot.append(name)
 		T.append(float(name.replace('scan_T','').replace('.dat','')))
@@ -56,7 +64,7 @@ b2 = []
 for index, file_b2 in enumerate(files_b2):
 	print('Procesing file: ', file_b2 )
 	with open(data_dir+file_b2, 'r') as file:
-		b2.append(json.load(file)['B2_reduced'])
+		b2.append(json.load(file)['B2']/B2_HS)
 ax.plot(T, b2, ms=5, marker='o', lw=1)
 #plt.ylim(-11,5)
 #plt.xlim(23,60)