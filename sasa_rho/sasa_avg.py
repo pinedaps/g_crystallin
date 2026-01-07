cb68037987131469cf855ae8b0aefe8ed982f585
Sebastian Pineda <sebastian@dell7050>
2026-01-07 13:43:39 +0100

    Implementing argparse in the sasa_avg.py, -i input sasa file and -o output filename. SASA average per aminoacid in 1AMM

--- sasa_rho/sasa_avg.py
+++ sasa_rho/sasa_avg.py
@@ -1,10 +1,16 @@
 import numpy as np
 from collections import defaultdict
+import argparse
 
-files = ["1XQ8_SASA","2NBI_SASA","6XRY_SASA"]
+parser = argparse.ArgumentParser(description="Average SASA")
+parser.add_argument("-i", type=str, help="input path")
+parser.add_argument("-o", type=str, help="ouput filename")
+args = parser.parse_args()
+
+files           = [args.i]
+output_filename = args.o
 
 data = defaultdict(list)
-
 for fname in files:
     with open(fname) as f:
         for line in f:
@@ -19,11 +25,23 @@ for fname in files:
             data[res].append(float(value))
 
 # write output
-with open("averaged_sasa", "w") as out:
+with open(output_filename, "w") as out:
     out.write("Residue  Mean  StdError\n")
     for res in sorted(data):
-        values = np.array(data[res])
-        mean = values.mean()
-        stderr = values.std(ddof=1) / np.sqrt(len(values))
+        values = np.asarray(data[res], dtype=float)
+        n = len(values)
+
+        if n == 0:
+            mean = np.nan
+            stderr = np.nan
+
+        elif n == 1:
+            mean = values[0]
+            stderr = 0.0   # no uncertainty with one sample
+
+        else:
+            mean = values.mean()
+            stderr = values.std(ddof=1) / np.sqrt(n)
+
         out.write(f"{res:4s} {mean:8.3f} {stderr:8.3f}\n")
 
